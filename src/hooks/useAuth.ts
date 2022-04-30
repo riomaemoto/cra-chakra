0a3a4ed2356f6fc0337e568209aa7b994fbcfbb7
Leo A <>
2022-05-01 00:09:12 +0900

    aaaa

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
 import { UseMessage } from "./use_message";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = UseMessage();
+  const { setLoginUser } = useLoginUser();
   const [loading, setLoading] = useState(false);
   const login = (id: string) => {
     setLoading(true);
@@ -14,14 +16,16 @@ export const useAuth = () => {
       .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
       .then((res) => {
         if (res.data) {
+          setLoginUser(res.data);
           showMessage({ title: "Loged in.", status: "success" });
           history.push("/home");
         } else {
           showMessage({ title: "Couldn't login ", status: "error" });
+          setLoading(false);
         }
       })
-      .catch(() => showMessage({ title: "User not found", status: "error" }))
-      .finally(() => setLoading(false));
+      .catch(() => showMessage({ title: "User not found", status: "error" }));
+    setLoading(false);
   };
 
   return { login, loading };
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,32 +5,34 @@ import { Login } from "../components/pages/login";
 import { HomeRoutes } from "./home_routes";
 import { Page404 } from "../components/pages/page404";
 import { HeaderLayout } from "../components/parts/layout/headerLayout";
-
+import { LoginUserProvider } from "../providers/loginUserProvider";
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route
-        path="/home"
-        render={({ match: { url } }) => (
-          <Switch>
-            {HomeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                <HeaderLayout>{route.children}</HeaderLayout>
-              </Route>
-            ))}
-          </Switch>
-        )}
-      />
-      <Route path={"*"}>
-        <Page404 />
-      </Route>
+      <LoginUserProvider>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route
+          path="/home"
+          render={({ match: { url } }) => (
+            <Switch>
+              {HomeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout>{route.children}</HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
+        <Route path={"*"}>
+          <Page404 />
+        </Route>
+      </LoginUserProvider>
     </Switch>
   );
 });
